cdaeb989a53c6c7d844fb8720e095dfaa1006f37
Michael Sumner <mdsumner@gmail.com>
2022-12-10 23:15:30 +1100

    internal

--- data-raw/city.R
+++ data-raw/city.R
@@ -2,3 +2,4 @@
 city <- tibble::as_tibble(maps::world.cities) |>  dplyr::transmute(name, lon = long, lat, country = country.etc, pop, capital)
 
 usethis::use_data(city, overwrite = TRUE)
+usethis::use_data(city, overwrite = TRUE, internal = TRUE)
